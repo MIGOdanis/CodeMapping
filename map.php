01fde3600088f3025b39282b158b39c973f82dc2
MIGOdanis <j0sdarg06@gmail.com>
2015-06-08 17:00:42 +0800

    TEST CM - 1

---
+++ map.php
@@ -0,0 +1,14 @@
+<?php
+$mapArray = array(
+	"4468" => 1002847, //CF-CM-TEST
+);
+if(isset($_GET['z'])){
+	$newZone = $mapArray[$_GET['z']];
+	if(isset($newZone) && $newZone > 0){
+		header('Content-Type: application/javascript');
+		echo "
+			document.write(\"<script type='text/javascript'>tosAdspaceInfo ={'aid':" . $newZone . ",'serverbaseurl':'tad.doublemax.net/','staticbaseurl':'static.doublemax.net/js/'}</script><script type='text/javascript' src='http://static.doublemax.net/js/tr.js'>\")
+		";
+	}
+}
+?>
--- rt_publisher.js
+++ rt_publisher.js
@@ -1,10 +1,12 @@
-console.log(rand());
-function rand(){
- 	return Math.floor(Math.max(0, Math.random() * (9+1)));
-}
-var cfadsn = "ad"+rand()+rand()+rand()+rand()+rand()+rand();
+var domain = "//103.17.10.57";
+
+// function rand(){
+//  	return Math.floor(Math.max(0, Math.random() * (9+1)));
+// }
+//var cfadsn = "ad"+rand()+rand()+rand()+rand()+rand()+rand();
 //document.write("<div id='"+cfadsn+"'></div>");
-document.write("<script src='//events.doublemax.net/adx/map.php?"+q+"&sn="+cfadsn+"'></script>");
+
+document.write("<script src='"+domain+"/adx/map.php?"+q+"''></script>");
 var cfadthispageurl = encodeURIComponent(location.href);
 var cfadelandimg = '<img src="//eland.doublemax.net/cfdmp/viewreceiver?'+q.replace('z', "p")+'&url='+cfadthispageurl+'" style="display: none;">'; 
 document.write(cfadelandimg);
