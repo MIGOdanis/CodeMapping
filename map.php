8d1397a6b755b70065ef3fa7d911f33180931e63
MIGOdanis <j0sdarg06@gmail.com>
2015-06-09 17:52:43 +0800

    FIX

--- map.php
+++ map.php
@@ -6,6 +6,7 @@ if(isset($_GET['z'])){
 		header('Content-Type: application/javascript');
 		echo "
 			document.write(\"<div><script type='text/javascript'>tosAdspaceInfo ={'aid':" . $newZone . ",'serverbaseurl':'tad.doublemax.net/','staticbaseurl':'static.doublemax.net/js/'}</script>\");
+			document.write(\"<script type='text/javascript' src='http://static.doublemax.net/js/tr.js'></div>\");
 			
 		";
 	}
