3c57c99ec84a4100f5aa65745d97643c8723991e
MIGOdanis <j0sdarg06@gmail.com>
2015-06-09 17:40:06 +0800

    CM TEST 1.4

--- map.php
+++ map.php
@@ -5,7 +5,9 @@ if(isset($_GET['z'])){
 	if(isset($newZone) && $newZone > 0){
 		header('Content-Type: application/javascript');
 		echo "
-			document.write(\"<script type='text/javascript'>tosAdspaceInfo ={'aid':" . $newZone . ",'serverbaseurl':'tad.doublemax.net/','staticbaseurl':'static.doublemax.net/js/'}</script><script type='text/javascript' src='http://static.doublemax.net/js/tr.js'>\")
+			document.write(\"<script type='text/javascript'>tosAdspaceInfo ={'aid':" . $newZone . ",'serverbaseurl':'tad.doublemax.net/','staticbaseurl':'static.doublemax.net/js/'}</script>\");
+			document.write(\"<script type='text/javascript' src='http://static.doublemax.net/js/tr.js'>\");
+			
 		";
 	}
 }
