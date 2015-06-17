6e728df9855b6413f79680ff6f59b3ba784d0120
MIGOdanis <j0sdarg06@gmail.com>
2015-06-17 16:20:20 +0800

    fix

--- delivery/index.php
+++ delivery/index.php
@@ -34,13 +34,16 @@ if(isset($newZone) && $newZone > 0){
 </html>
 <?php
 }elseif(isset($_GET['jscript'])){
-	if(isset($newZone) && $newZone > 0){
-		header('Content-Type: application/javascript');
-		echo "
-			document.write(\"<div><script type='text/javascript'>tosAdspaceInfo ={'aid':" . $newZone . ",'serverbaseurl':'tad.doublemax.net/','staticbaseurl':'static.doublemax.net/js/'}</script>\");
-			document.write(\"<script type='text/javascript' src='http://static.doublemax.net/js/tr.js'></script></div>\");
-			
-		";
+	if(isset($_GET['z'])){
+		$newZone = $mapArray[$_GET['z']];
+		if(isset($newZone) && $newZone > 0){
+			header('Content-Type: application/javascript');
+			echo "
+				document.write(\"<div><script type='text/javascript'>tosAdspaceInfo ={'aid':" . $newZone . ",'serverbaseurl':'tad.doublemax.net/','staticbaseurl':'static.doublemax.net/js/'}</script>\");
+				document.write(\"<script type='text/javascript' src='http://static.doublemax.net/js/tr.js'></script></div>\");
+				
+			";
+		}
 	}
 }else{
 	$json['status'] = 12;
